a2d061a2fe9f68e5e1f06b5b4d5eb6fa86eeea82
articodetr <info@articode.com.tr>
2026-02-07 19:04:32 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      include: ['stream', 'buffer', 'crypto', 'process', 'util'],
-    }),
-  ],
+  plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-    include: ['exceljs'],
   },
 });

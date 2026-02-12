6fe7a5167aa0c28b5164f36435b534ab618f1607
articodetr <info@articode.com.tr>
2026-02-12 22:40:52 +0300

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -63,7 +63,7 @@ export default function Home() {
 
   useEffect(() => {
     fetchStats();
-    fetchHeroSlides();
+    fetchHeroImage();
     fetchUpcomingEvents();
     fetchLatestNews();
   }, [language]);
